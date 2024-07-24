27a887b0ebae91561928ad67e82cbfdcaf0e8178
Lady Killerr <dungvuduc0509@gmail.com>
2024-07-24 15:48:38 +0700

    feat: added enemy path and functional enemies

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> enemyPath = new List<Waypoint>();
+    [SerializeField] [InspectorRange(0f,5f)]float movementSpeed = 1f;
+
+    private void Update()
+    {
+        
+    }
+
+    void Start()
+    {
+        StartCoroutine(ListWaypointCoordinates());
+
+
+    }
+
+    IEnumerator ListWaypointCoordinates()
+    {
+        foreach (Waypoint waypoint in enemyPath)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+
+            transform.LookAt(endPosition);
+
+            float travelPercent = 0f;
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * movementSpeed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+        }
+    }
+}
